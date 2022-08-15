6a21535fcb8f53caf63e1306191b007ad976cf3a
unknown <fabian179rc@gmail.com>
2022-08-15 20:48:36 -0300

    modificacion de accestoken en env

--- api/src/routes/mercadopago.ts
+++ api/src/routes/mercadopago.ts
@@ -1,20 +1,22 @@
 const { Router } = require('express');
 const mp = require("mercadopago");
 const mercadopago = Router();
+require('dotenv').config();
+const PROD_ACCESS_TOKEN = process.env.ACCESS_TOKEN
 
-mp.configure({ //cuenta produccion acces token test 1 (TETE8284659    qatest1769)
-    access_token: "APP_USR-6913287203050942-081213-9ae4b41c5f23db785ed7c59bdbb34d5e-1178359030", //PROD_ACCESS_TOKEN
+mp.configure({ //cuenta produccion access token test 1 (TETE8284659    qatest1769)
+    access_token: PROD_ACCESS_TOKEN, //PROD_ACCESS_TOKEN
   });
 
 mercadopago.post('/checkout', (req,res)=>{
 
     let preference = {
         items: [  //pueden ser varios items
-          {
-            title: req.body.title,
-            unit_price: parseInt(req.body.price), //le llega como string
-            quantity: 1,
-          },
+        {
+          title: req.body.title,
+          unit_price: parseInt(req.body.price), //le llega como string
+          quantity: 1,
+        },
           // {
           //   title: "stars",
           //   unit_price: 1200,
