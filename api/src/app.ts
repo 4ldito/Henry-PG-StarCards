16e6fd7b0503beddb3c2f9fb47b1fe657c531d25
unknown <antudippolito123@gmail.com>
2022-08-15 15:59:56 -0300

    fixed porblems in app.js

--- api/src/app.ts
+++ api/src/app.ts
@@ -14,7 +14,7 @@ server.use(express.urlencoded({ extended: true, limit: '50mb' }))
 server.use(express.json({ limit: '50mb' }))
 server.use(cookieParser())
 server.use(morgan('dev'))
-server.use((_, res, next) => {
+server.use((_:any, res:any, next:any) => {
   res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
