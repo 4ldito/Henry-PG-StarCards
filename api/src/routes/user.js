383c9a5ebc1cfa973dab31116a64df2f483df46d
unknown <fabian179rc@gmail.com>
2022-08-17 14:00:42 -0300

    ruta user

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const User = require("../models/user");
 // const { Country, Activity, cache } = require('../db');
 
 // const axios = require('axios');
@@ -8,12 +9,74 @@ const userRoute = Router();
 
 ///////////////////////Routes Profile//////////////////////////////////////////
 
-userRoute.get("/", (req, res) => {
+userRoute.patch("/changeprofile:username", async(req, res, next) => {
 try {
-    return res.send("<h1>Hola user!</h1>");
-  
+const usernameNew = req.body
+    await User.update(User, {
+        where: {
+            username : usernameNew 
+        }
+    })
+    res.json('Username Updated!')
+
 } catch (error) {
-  
+      next(err)
+}});
+
+userRoute.patch("/changeprofile:email", async(req, res, next) => {
+try {
+const emailNew = req.body
+    await User.update(User, {
+        where: {
+            email : emailNew 
+        }
+    })
+    res.json('Email Updated!')
+
+} catch (error) {
+      next(err)
+}});
+
+userRoute.patch("/changeprofile:password", async(req, res, next) => {
+try {
+const passwordNew = req.body
+    await User.update(User, {
+        where: {
+            password : passwordNew 
+        }
+    })
+    res.json('Password Updated!')
+
+} catch (error) {
+      next(err)
+}});
+
+userRoute.patch("/changeprofile:profileImg", async(req, res, next) => {
+try {
+const profileImgNew = req.body
+    await User.update(User, {
+        where: {
+            profileImg : profileImgNew 
+        }
+    })
+    res.json('Profile Image Updated!')
+
+} catch (error) {
+      next(err)
+}});
+
+userRoute.patch("/changeprofile:coverImg", async(req, res, next) => {
+try {
+const coverImgNew = req.body
+    await User.update(User, {
+        where: {
+            coverImg : coverImgNew 
+        }
+    })
+    res.json('Cover Image Updated!')
+
+} catch (error) {
+      next(err)
 }});
 
 module.exports = userRoute;
