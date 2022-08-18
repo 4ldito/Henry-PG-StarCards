90f0d05512400017c1daeb7381b7e344efd491e8
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-08-18 13:12:20 -0300

    changed route

--- client/src/redux/actions/starsPacks.js
+++ client/src/redux/actions/starsPacks.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-import { GET_ALL_STARS_PACKS } from "./actionTypes";
+import axios from 'axios'
+import { GET_ALL_STARS_PACKS } from './actionTypes'
 
-export function getStarsPacks() {
+export function getStarsPacks () {
   return async function (dispatch) {
-    const response = await axios.get("http://localhost:3001/stars-pack/all");
-    dispatch({ type: GET_ALL_STARS_PACKS, payload: response.data });
-  };
+    const response = await axios.get('http://localhost:3001/stars-pack/all')
+    dispatch({ type: GET_ALL_STARS_PACKS, payload: response.data })
+  }
 }
