203f64d7c6ea31838ecb45f7d8ff8f3b25596926
NoahReaver <emilianojaparicio@gmail.com>
2022-08-18 11:49:08 -0300

    pack routes

--- api/src/routes/starsPacks.js
+++ api/src/routes/starsPacks.js
@@ -1,22 +1,32 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-const { Router } = require('express')
-const db = require('../db')
-const { StarsPack } = db
+const { Router } = require("express");
+const db = require("../db");
+const { StarsPack } = db;
 
-const starsPackRoute = Router()
+const starsPackRoute = Router();
 
 const getAllStarsPack = async () => {
-  const allPacksDB = await StarsPack.findAll()
-  return allPacksDB
-}
+  const allPacksDB = await StarsPack.findAll();
+  return allPacksDB;
+};
 
-starsPackRoute.get('/', async (req, res, next) => {
+starsPackRoute.get("/all", async (req, res, next) => {
   try {
-    const packs = await getAllStarsPack()
-    return res.send(packs)
+    const packs = await getAllStarsPack();
+    return res.send(packs);
   } catch (error) {
-    return next(error)
+    return next(error);
   }
-})
+});
 
-module.exports = starsPackRoute
+starsPackRoute.get("/:status", async (req, res, next) => {
+  const { status } = req.params;
+  try {
+    const packs = await getAllStarsPack({ where: { StatusId: status } });
+    return res.send(packs);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = starsPackRoute;
