db37e1344575ffc6bbfac270b534ab673c2f1089
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-08-17 21:10:51 -0300

    refactorized code

--- client/src/Components/Shop/ShopCart/ShopCart.jsx
+++ client/src/Components/Shop/ShopCart/ShopCart.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Mercadopago from './Mercadopago'
 
 const ShopCart = () => {
-  // const dispatch = useDispatch()
   const { starsPack, cardsPack } = useSelector(state => state.shopCartReducer.shopCart)
   let total = 0
   return (
@@ -12,30 +10,32 @@ const ShopCart = () => {
       {starsPack.length > 0 || cardsPack.length > 0
         ? (
           <>
-            {starsPack.length > 0 && <div>
-              <h2>Carrito de stars</h2>
-              {starsPack.map(item => {
-                total += item.price * item.quantity
-                return (
-                  <div key={item.id}>
-                    <p>{item.name} Cantidad: {item.quantity} Subtotal: ARS ${item.price * item.quantity}</p>
-                  </div>
-                )
-              })}
-              <p>Total: ARS ${total}</p>
-              <Mercadopago shopCartItems={starsPack} />
-            </div>}
-            {cardsPack.length && <div>
-              <h2>Carrito de packs cards</h2>
-              {cardsPack.map(item => {
-                return (
-                  <div key={item.id}>
-                    <p>{item.name}</p>
-                  </div>
-                )
-              })}
-              <button>Comprar packs de cards</button>
-            </div>}
+            {starsPack.length > 0 && (
+              <div>
+                <h2>Carrito de stars</h2>
+                {starsPack.map(item => {
+                  total += item.price * item.quantity
+                  return (
+                    <div key={item.id}>
+                      <p>{item.name} Cantidad: {item.quantity} Subtotal: ARS ${item.price * item.quantity}</p>
+                    </div>
+                  )
+                })}
+                <p>Total: ARS ${total}</p>
+                <Mercadopago shopCartItems={starsPack} />
+              </div>)}
+            {cardsPack.length && (
+              <div>
+                <h2>Carrito de packs cards</h2>
+                {cardsPack.map(item => {
+                  return (
+                    <div key={item.id}>
+                      <p>{item.name}</p>
+                    </div>
+                  )
+                })}
+                <button>Comprar packs de cards</button>
+              </div>)}
           </>)
         : <p>El carrito esta vacio</p>}
     </div>
