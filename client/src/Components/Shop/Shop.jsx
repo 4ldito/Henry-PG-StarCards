4d2b113828d1946e139799bed196a21d1147b1a8
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-08-19 18:44:57 -0300

    added css

--- client/src/Components/Shop/Shop.jsx
+++ client/src/Components/Shop/Shop.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react'
-import { useFetchStarsPack } from '../../hooks/useFetchStarsPack'
-import { useSelector, useDispatch } from 'react-redux'
-import { removeFromShopCart } from './../../redux/actions/shopCart'
-import { Link } from 'react-router-dom'
-import Swal from 'sweetalert2'
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
+import { useSelector, useDispatch } from 'react-redux';
 
-import Packs from './Packs/Packs'
+import { useFetchStarsPack } from '../../hooks/useFetchStarsPack';
 import { useFetchCardsPack } from './../../hooks/useFetchCardsPack';
-import { cleanMsgInfo } from '../../redux/actions/cardsPack'
+import { cleanMsgInfo } from '../../redux/actions/cardsPack';
+
+import Packs from './Packs/Packs';
+
+import style from './styles/Shop.module.css';
+import Filters from './Filters';
 
 const Shop = () => {
   const dispatch = useDispatch()
 
   const loadedStarsPack = useFetchStarsPack().loaded
   const loadCardsPack = useFetchCardsPack().loaded
-  const { starsPack, cardsPack } = useSelector(state => state.shopCartReducer.shopCart)
 
   const msgInfoPurchase = useSelector(state => state.cardsPacksReducer.msg)
 
@@ -30,36 +31,15 @@ const Shop = () => {
 
   useEffect(() => {
     return () => {
-      dispatch(cleanMsgInfo())
+      dispatch(cleanMsgInfo());
     }
   }, []);
-
-  const handleRemoveItem = (e, type) => {
-    e.preventDefault()
-    const target = Number(e.target.id)
-    dispatch(removeFromShopCart(target, type))
-  }
 
   if (!loadedStarsPack || !loadCardsPack) return (<p>Loading..</p>)
 
   return (
-    <div>
-      <Link to='/shopcart'>Ir al carrito</Link>
-      <h2>Carrito: </h2>
-      <h3>Carrito de Stars Packs</h3>
-      {starsPack.map(item =>
-        <div key={item.id}>
-          <p>{item.name} cantidad: {item.quantity}</p>
-          <button id={item.id} onClick={(e) => handleRemoveItem(e, 'starsPack')}>Eliminar del carrito</button>
-        </div>
-      )}
-      <h3>Carrito de Cards Packs</h3>
-      {cardsPack.map(item =>
-        <div key={item.id}>
-          <p>{item.name} cantidad: {item.quantity}</p>
-          <button id={item.id} onClick={(e) => handleRemoveItem(e, 'cardsPack')}>Eliminar del carrito</button>
-        </div>
-      )}
+    <div className={style.container}>
+      <Filters />
       <Packs type='starsPack' />
       <Packs type='cardsPack' />
     </div>
