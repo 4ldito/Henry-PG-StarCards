bc761b67fea9d6e754a2f1604c07526cd9cc5ffd
Emerson Villalta <reaperfocs@gmail.com>
2022-08-19 16:47:25 -0500

    pruebas

--- client/src/Components/Registro/Login.jsx
+++ client/src/Components/Registro/Login.jsx
@@ -1,116 +1,68 @@
-import React, {useState} from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+import React, { useState } from 'react'
 import axios from 'axios'
-// import css from './Registro.module.css'
+import { signIn } from '../../redux/actions/user'
 import './login.css'
+import { useDispatch } from 'react-redux'
 
-export default function Login () {
+export default function Login() {
   /*const { loginWithRedirect } = useAuth0()
   return (
     <button onClick={() => loginWithRedirect()}>Login</button>
   )
     */
-  
-    // React States
-    const [errorMessages, setErrorMessages] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
-  
-    const [loginUsername, setLoginUsername] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
-    const [loginEmail, setLoginEmail] = useState("");
-    
-    
+  const dispatch = useDispatch();
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    // User Login info
+  const [input, setInput] = useState({
+    loginPassword: '',
+    loginEmail: ''
+  });
+  // User Login info
 
-    const login = () => {
-      Axios({
-        method: "POST",
-        data: {
-          email: 'email', 
-          username: loginUsername,
-          password: loginPassword,
-        },
-        withCredentials: true,
-        url: "http://localhost:3001/signup",
-      }).then((res) => console.log(res));
-    };
+  const login = (e) => {
+    e.preventDefault();
+    dispatch(signIn({ loginEmail, loginUsername, loginPassword }));
+  };
 
-    const database = [
-      {
-        username: "user1",
-        password: "pass1"
-      },
-      {
-        username: "user2",
-        password: "pass2"
-      }
-    ];
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+
   
-    const errors = {
-      uname: "invalid username",
-      pass: "invalid password"
-    };
-  
-    const handleSubmit = (event) => {
-      //Prevent page reload
-      event.preventDefault();
-  
-      var { uname, pass } = document.forms[0];
-  
-      // Find user login info
-      const userData = database.find((user) => user.username === uname.value);
-  
-      // Compare user info
-      if (userData) {
-        if (userData.password !== pass.value) {
-          // Invalid password
-          setErrorMessages({ name: "pass", message: errors.pass });
-        } else {
-          setIsSubmitted(true);
-        }
-      } else {
-        // Username not found
-        setErrorMessages({ name: "uname", message: errors.uname });
-      }
-    };
-  
-    // Generate JSX code for error message
-    const renderErrorMessage = (name) =>
-      name === errorMessages.name && (
-        <div className="error">{errorMessages.message}</div>
-      );
-  
-    // JSX code for login form
-    const renderForm = (
-      <div className="form">
-        <form onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label>Username </label>
-            <input type="text" name="uname" required />
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input type="password" name="pass" required />
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-            <input type="submit" />
-          </div>
-        </form>
-      </div>
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
     );
+
+
   
-    return (
-      <div className="appli">
-        <div className="login-form">
-          <div className="title">Sign In</div>
-          {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+
+  return (<div className="form">
+      <form onSubmit={(e)=>{login(e)}}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="email" name="email" onChange={handleOnChange} required />
+          {renderErrorMessage("uname")}
         </div>
-      </div>
-    );
-   
-    
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" onChange={handleOnChange} required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input  type="submit" />
+        </div>
+      </form>
+    </div>)
   
-}
+
+
+    }
