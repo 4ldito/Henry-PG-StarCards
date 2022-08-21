5a0c9e2300d323ebda6bb5fc9cdb647c73e02d51
unknown <antudippolito123@gmail.com>
2022-08-21 01:40:36 -0300

    cambios minmos

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 import UserOptions from "./UserOptions/UserOptions";
 
 import css from "./Nav.module.css";
+import useValidToken from "../../hooks/useValidToken";
 
 export default function Nav() {
-  const isLogged = useSelector(state => state.userReducer.token)
+  const validLogged = useValidToken({navigate: false})
   const [visibleUserOptions, setVisibleUserOptions] = useState(false);
   return (
     <div className={css.nav}>
@@ -22,9 +23,9 @@ export default function Nav() {
           </Link>
         </li>
         <li className={css.li}>
-          <Link className={css.link} to="/playroom">
+          {validLogged?<Link className={css.link} to="/playroom">
             Playroom
-          </Link>
+          </Link>:<></>}
         </li>
         <li className={css.li}>
           <Link className={css.link} to="/about">
@@ -45,7 +46,7 @@ export default function Nav() {
 
       {visibleUserOptions ? (
         <div>
-          <UserOptions isLogged = {isLogged}/>{" "}
+          <UserOptions isLogged = {validLogged}/>{" "}
         </div>
       ) : (
         <></>
