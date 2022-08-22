eb045e921933580e6ba95affed1c133f8471cf1a
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-08-21 23:58:18 -0300

    a

--- client/src/Components/Shop/PurchaseCompleted.jsx
+++ client/src/Components/Shop/PurchaseCompleted.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom'
-import { getPurchaseInfo } from '../../redux/actions/shopCart';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { purchaseCompleted } from '../../redux/actions/user';
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { getPurchaseInfo } from "../../redux/actions/shopCart";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { purchaseCompleted } from "../../redux/actions/user";
+
+import css from "./styles/PurchaseCompleted.module.css";
 
 const PurchaseCompleted = () => {
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
-  const paymentId = searchParams.get('payment_id');
+  const paymentId = searchParams.get("payment_id");
 
-  const purchaseInfo = useSelector(state => state.shopCartReducer.purchaseInfo)
+  const purchaseInfo = useSelector(
+    (state) => state.shopCartReducer.purchaseInfo
+  );
 
   useEffect(() => {
     dispatch(getPurchaseInfo(paymentId));
@@ -18,16 +22,18 @@ const PurchaseCompleted = () => {
 
   useEffect(() => {
     if (purchaseInfo.userId) {
-      dispatch(purchaseCompleted(purchaseInfo.userId, purchaseInfo.items))
+      dispatch(purchaseCompleted(purchaseInfo.userId, purchaseInfo.items));
       dispatch(getPurchaseInfo());
     }
   }, [purchaseInfo]);
 
   return (
-    <div>PurchaseCompleted
-      <p>Tu compra esta </p>
+    <div className={css.container}>
+      <div className={css.model}>
+        <h1 className={css.h1}>Tu compra se completo con éxito</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PurchaseCompleted
+export default PurchaseCompleted;
