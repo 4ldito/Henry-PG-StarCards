f187fc37e4caccefae6fd629f73fa32741c01607
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-08-21 22:16:22 -0300

    added styles to shopcart and added normalize

--- client/src/Components/Shop/ShopCart/ShopCart.jsx
+++ client/src/Components/Shop/ShopCart/ShopCart.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
 
 import Mercadopago from './Mercadopago';
 import Swal from 'sweetalert2';
 
 import { cleanPreferenceId, removeFromShopCart, shopcartBuyCardsPacks, shopCartCleanMsgInfo } from './../../../redux/actions/shopCart';
 
-import style from '../../../styles/shopCart/shopCart.module.css';
+import style from '../styles/ShopCart.module.css';
 
 const ShopCart = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const { starsPack, cardsPack } = useSelector(state => state.shopCartReducer.shopCart)
   const msgInfoPurchase = useSelector(state => state.shopCartReducer.msg)
@@ -56,44 +58,53 @@ const ShopCart = () => {
   }, [user?.stars])
 
   return (
-    <div className={style.container}>ShopCart
-      {starsPack.length > 0 || cardsPack.length > 0
-        ? (
-          <>
-            {starsPack.length > 0 && (
-              <div>
-                <h2>Carrito de stars</h2>
-                {starsPack.map(item => {
-                  totalStarsPack += item.price * item.quantity
-                  return (
-                    <div key={item.id}>
-                      <p>{item.name} Cantidad: {item.quantity} Subtotal: ARS ${item.price * item.quantity}</p>
-                      <button onClick={(e) => handleRemoveItem(e, 'starsPack')} id={item.id}>X</button>
-                    </div>
-                  )
-                })}
-                <p>Total: ARS ${totalStarsPack}</p>
-                <Mercadopago shopCartItems={starsPack} />
-              </div>)}
-            {cardsPack.length > 0 && (
-              <div>
-                <h2>Carrito de packs cards</h2>
-                {cardsPack.map(item => {
-                  const subtotal = item.price * item.quantity
-                  totalCardsPack += subtotal
-                  return (
-                    <div key={item.id}>
-                      <p>{item.name}</p>
-                      <p>Precio: {item.price} Stars - Cantidad: {item.quantity} - Subtotal: {subtotal} Stars</p>
-                      <button onClick={(e) => handleRemoveItem(e, 'cardsPack')} id={item.id}>X</button>
-                    </div>
-                  )
-                })}
-                <p>Total: {totalCardsPack} Stars</p>
-                <button onClick={handleBuyCardsPack} disabled={!buyWithStars}>{buyWithStars ? "Comprar packs de cards" : "Tus Stars son insuficientes"}</button>
-              </div>)}
-          </>)
-        : <p>El carrito esta vacio</p>}
+    <div className={style.container}>
+      <div className={style.infoContainer}>
+        <h2>Carrito</h2>
+        {starsPack.length > 0 || cardsPack.length > 0
+          ? (
+            <>
+              {starsPack.length > 0 && (
+                <div className={style.containerCart}>
+                  <h2>Carrito de stars</h2>
+                  {starsPack.map(item => {
+                    totalStarsPack += item.price * item.quantity
+                    return (
+                      <div className={style.containerItem} key={item.id}>
+                        <p>{item.name}</p>
+                        <p>Precio: ${item.price} ARS</p>
+                        <p>Cantidad: {item.quantity} </p>
+                        <p>Subtotal: ARS ${item.price * item.quantity}</p>
+                        <button onClick={(e) => handleRemoveItem(e, 'starsPack')} id={item.id}>X</button>
+                      </div>
+                    )
+                  })}
+                  <p>Total: ARS ${totalStarsPack}</p>
+                  {user.id ? <Mercadopago shopCartItems={starsPack} /> : <button onClick={() => { navigate("/login") }}>Logeate</button>}
+                </div>)}
+              {cardsPack.length > 0 && (
+                <div className={style.containerCart}>
+                  <h2>Carrito de packs cards</h2>
+                  {cardsPack.map(item => {
+                    const subtotal = item.price * item.quantity
+                    totalCardsPack += subtotal
+                    return (
+                      <div className={style.containerItem} key={item.id}>
+                        <p>{item.name}</p>
+                        <p>Precio: {item.price} Stars</p>
+                        <p>Cantidad: {item.quantity}</p>
+                        <p>Subtotal: {subtotal} Stars</p>
+                        <button onClick={(e) => handleRemoveItem(e, 'cardsPack')} id={item.id}>X</button>
+                      </div>
+                    )
+                  })}
+                  <p>Total: {totalCardsPack} Stars</p>
+                  {user.id ? <button onClick={handleBuyCardsPack} disabled={!buyWithStars}>{buyWithStars ? "Comprar packs de cards" : "Tus Stars son insuficientes"}</button>
+                    : <button onClick={() => { navigate("/login") }}>Logeate</button>}
+                </div>)}
+            </>)
+          : <p>El carrito esta vacio</p>}
+      </div>
     </div>
   )
 }
