2fba95eeb49e8fdd511c8cd0e2a17dd1898cdfe1
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-08-24 21:39:07 -0300

    dale antu como vas a pensar que va a funcionar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,9 +10,7 @@ import axios from 'axios';
 let api = {
   backUrl: import.meta.env.VITE_BASE_URL
 };
-
 axios.defaults.baseURL = api.backUrl
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter >
