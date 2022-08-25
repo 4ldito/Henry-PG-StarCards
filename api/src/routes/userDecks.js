b3dcc31c81c4e05561a20406b2b4a836bc945cd5
unknown <antudippolito123@gmail.com>
2022-08-25 12:33:49 -0300

    hechas las rutas del back de creacion y peticion de mazo

--- api/src/routes/userDecks.js
+++ api/src/routes/userDecks.js
@@ -1,27 +1,62 @@
 const { Router } = require("express");
 const db = require("../db");
+const userCards = require("../models/userCards");
 
-const { User, Deck } = db;
+const { User, Deck, UserCards } = db;
 
 const userDecksRoute = Router();
 
-userDecksRoute.get('/:id', async (req, res, next) => {
+userDecksRoute.get('/:userId', async (req, res, next) => {
     try {
-        const { id } = req.params;
-        console.log(id);
-        const user = await User.findByPk(id, { include: Deck });
-
-        res.json(user.Decks);
+        const { userId } = req.params;
+        const { deckId } = req.query;
+        const user = await User.findByPk(userId, { include: Deck });
+        if (!user) return res.json({ error: "El usuario no existe" });
+        if (!deckId) {
+            let promisedDecks = user.Decks.map(async e => {
+                return Deck.findByPk(e.id, { include: UserCards });
+            });
+            const decks = await Promise.all(promisedDecks);
+            return res.json(decks);
+        }
+        const deckFound = await Deck.findByPk(deckId, { include: UserCards });
+        if(!deckFound)return res.json({error:'El mazo no existe'});
+        return res.json(deckFound);
     } catch (err) {
         next(err);
     }
 });
-userDecksRoute.post('/', async (req, res, next) => {
+userDecksRoute.post('/:userId', async (req, res, next) => {
+    const { newDeckCards, name, race } = req.body;
+    const userId = req.params.userId;
+    if (newDeckCards.length !== 20) return res.json({ error: "El mazo debe tener al menos 20 cartas" });
+    try {
+
+        const newDeck = await Deck.create({ name, race }, { include: [UserCards] });
+        newDeckCards.forEach(async e => {
+            const userCard = await UserCards.findByPk(e.id, { include: [Deck] });
+            await userCard.addDeck(newDeck);
+            await newDeck.addUserCards(userCard);
+            await userCard.save();
+            await newDeck.save();
+        })
+        const user = await User.findByPk(userId, { include: Deck });
+        user.addDeck(newDeck);
+        user.save();
+        res.json('creado');
+    } catch (err) {
+        next(err);
+    }
+});
+
+userDecksRoute.put((req,res,next)=>{
     try{
 
     }catch(err){
         next(err);
     }
-});
+})
+
+
 
 module.exports = userDecksRoute;
