022480a3f627886a1bdc229108d84a29d76ed43c
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-08-27 17:47:45 -0300

    Basic inventory styles. LP

--- client/src/components/UserProfile/Inventory/InventoryContainer.jsx
+++ client/src/components/UserProfile/Inventory/InventoryContainer.jsx
@@ -1,47 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { getUserCards } from '../../../redux/actions/cards/userCards';
-import { CardContainer } from '../../Card/CardContainer';
-import DeckList from './Decks/DeckList';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserCards } from "../../../redux/actions/cards/userCards";
+import { CardContainer } from "../../Card/CardContainer";
+import DeckList from "./Decks/DeckList";
 import css from "./Inventory.module.css";
-
-
 
 export default function InventoryContainer() {
   const dispatch = useDispatch();
-  const filteredUserCards = useSelector(state => state.album.filteredUserCards)
+  const filteredUserCards = useSelector(
+    (state) => state.album.filteredUserCards
+  );
   const cards = useSelector((state) => state.album.cards);
-  const user = useSelector(state => state.userReducer.user);
-  const [creationFormIsActive,setCreationFormIsActive] = useState(false);
-  const [newDeckCards,setNewDeckCards] = useState([]);
-  const [actualStackToShow,setActualStackToShow] = useState([]);
+  const user = useSelector((state) => state.userReducer.user);
+  const [creationFormIsActive, setCreationFormIsActive] = useState(false);
+  const [newDeckCards, setNewDeckCards] = useState([]);
+  const [actualStackToShow, setActualStackToShow] = useState([]);
   function renderNotRepeat() {
     let cartas = [];
-    filteredUserCards?.forEach(e=> {
-      cartas.push(<div key={e.id}><CardContainer  card={e} repeat={e.repeat}/></div>)
-    })
-    if(filteredUserCards.length)return cartas
-    return <label>Not cards found</label> 
+    filteredUserCards?.forEach((e) => {
+      cartas.push(
+        <div key={e.id}>
+          <CardContainer card={e} repeat={e.repeat} />
+        </div>
+      );
+    });
+    if (filteredUserCards.length) return cartas;
+    return <label>Not cards found</label>;
   }
-
 
   useEffect(() => {
-    dispatch(getUserCards(user.UserCards, cards))
+    dispatch(getUserCards(user.UserCards, cards));
   }, [cards]);
 
-  function setVisibleStack(name){
-    if(actualStackToShow.includes(name)){
-      setActualStackToShow(actualStackToShow.filter(e=>e!==name));
-    }else{
-      setActualStackToShow([...actualStackToShow,name])
+  function setVisibleStack(name) {
+    if (actualStackToShow.includes(name)) {
+      setActualStackToShow(actualStackToShow.filter((e) => e !== name));
+    } else {
+      setActualStackToShow([...actualStackToShow, name]);
     }
   }
-  
-  return (<div className={css.InventoryContainer}>
-    <button name='cartas' onClick={(e)=>{setVisibleStack(e.target.name)}}>Cartas</button>
-    <button name='mazos' onClick={(e)=>{setVisibleStack(e.target.name)}}>Mazos</button>
-    {actualStackToShow.includes('cartas')?<div className={css.cartas}>{renderNotRepeat()}</div>:<></>}
-    {actualStackToShow.includes('mazos')?<DeckList showCards={setActualStackToShow} userId={user.id}></DeckList>:<></>}
-    
-  </div >);
+
+  return (
+    <div className={css.inventoryContainer}>
+      <div className={css.buttons}>
+        <button
+          name="cartas"
+          className={css.btnCards}
+          onClick={(e) => {
+            setVisibleStack(e.target.name);
+          }}
+        >
+          Cartas
+        </button>
+        <button
+          name="mazos"
+          className={css.btnDecks}
+          onClick={(e) => {
+            setVisibleStack(e.target.name);
+          }}
+        >
+          Mazos
+        </button>
+      </div>
+
+      {actualStackToShow.includes("cartas") ? (
+        <div className={css.cartas}>{renderNotRepeat()}</div>
+      ) : (
+        <></>
+      )}
+      {actualStackToShow.includes("mazos") ? (
+        <DeckList showCards={setActualStackToShow} userId={user.id}></DeckList>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
