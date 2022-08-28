cf754d4a323e20d026f8c5c091825739fd53fc1b
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-08-28 12:45:56 -0300

    removed import with components with C

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-// import ProtectedRoutes from './ProtectedRoutes'
-// import Registro from './components/Registro/Registro'
 import Login from './components/Registro/Login'
 import UserProfile from "./components/UserProfile/UserProfile";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Playroom from "./components/Playroom/Playroom";
-// import ShopCart from "./components/Shop/ShopCart/ShopCart";
 import Shop from "./components/Shop/Shop";
 import PurchaseCompleted from "./components/Shop/PurchaseCompleted";
 import Nav from "./components/Nav/Nav";
 import About from "./components/About/About";
 import PrivateChat from "./components/UserProfile/PrivateChat/PrivateChat";
-
-import "./App.css";
 import { resetReduxState } from "./redux/actions";
 import Inventory from "./components/UserProfile/Inventory/Inventory";
-// import { setToken } from './redux/actions/user'
-
-
 import RecoveryPassword from './components/Registro/RecoveryPassword';
-import Registro from "./components/Registro/Registro";import Firebase from "./Components/FirebaseAdmi/Firebase";
+import Registro from "./components/Registro/Registro";
+import Firebase from "./Components/FirebaseAdmi/Firebase";
 
+import "./App.css";
 
 function App() {
-
   const dispatch = useDispatch();
-
   function handleKeyboard(e) {
     if (e.repeat) return;
     if ((e.metaKey || e.ctrlKey) && e.key === "x") {
@@ -44,7 +36,7 @@ function App() {
     <div>
       <Nav />
       <Routes>
-        <Route path="/firebase" element={<Firebase />}/>
+        <Route path="/firebase" element={<Firebase />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Registro />} />
         <Route path="/login" element={<Login />} />
@@ -52,12 +44,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/purchase-completed" element={<PurchaseCompleted />} />
         <Route path="/playroom" element={<Playroom />} />
-        {/* <Route path="/shopcart" element={<ShopCart />} /> */}
         <Route path="/userProfile" element={<UserProfile />} />
-
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/recovery" element={<RecoveryPassword />} />
-        {/* </Route> */}
         <Route path="/privateChat" element={<PrivateChat />} />
       </Routes>
     </div>
