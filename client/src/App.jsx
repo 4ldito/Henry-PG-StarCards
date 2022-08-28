651cda8c0ad11a2ed3d520022f94886705552995
unknown <fabian179rc@gmail.com>
2022-08-28 19:11:32 -0300

    redireccionamiento logo arreglado segun si esta logeado o no

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,24 @@
 import React, { useEffect } from "react";
 import { useDispatch } from 'react-redux'
 import { Route, Routes } from "react-router-dom";
-// import ProtectedRoutes from './ProtectedRoutes'
-// import Registro from './components/Registro/Registro'
 import Login from './components/Registro/Login'
 import UserProfile from "./components/UserProfile/UserProfile";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Playroom from "./components/Playroom/Playroom";
-// import ShopCart from "./components/Shop/ShopCart/ShopCart";
 import Shop from "./components/Shop/Shop";
 import PurchaseCompleted from "./components/Shop/PurchaseCompleted";
 import Nav from "./components/Nav/Nav";
 import About from "./components/About/About";
-
-import "./App.css";
 import { resetReduxState } from "./redux/actions";
-import Inventory from "./components/UserProfile/Inventory/Inventory";
-// import { setToken } from './redux/actions/user'
-
 import RecoveryPassword from './components/Registro/RecoveryPassword';
 import Registro from './components/Registro/Registro';
+import "./App.css";
 
+// import ProtectedRoutes from './ProtectedRoutes'
+// import Registro from './components/Registro/Registro'
+// import Inventory from "./components/UserProfile/Inventory/Inventory";
+// import { setToken } from './redux/actions/user'
+// import ShopCart from "./components/Shop/ShopCart/ShopCart";
 
 function App() {
 
@@ -44,15 +42,15 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path='/register' element={<Registro />} />
+        <Route path="/recovery" element={<RecoveryPassword />} />
         <Route path='/login' element={<Login />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/purchase-completed" element={<PurchaseCompleted />} />
         <Route path='/playroom' element={<Playroom />} />
-        {/* <Route path='/shopcart' element={<ShopCart />} /> */}
         <Route path="/userProfile" element={<UserProfile />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/recovery" element={<RecoveryPassword />} />
+        {/* <Route path='/shopcart' element={<ShopCart />} /> */}
+        {/* <Route path="/inventory" element={<Inventory />} /> */}
         {/* </Route> */}
       </Routes>
     </div>
