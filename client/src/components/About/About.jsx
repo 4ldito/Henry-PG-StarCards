3e97a8888a0b367b6260b4c16740258c84c72bcc
NoahReaver <emilianojaparicio@gmail.com>
2022-08-29 11:20:09 -0300

    Components bye

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import css from "./About.module.css";
+
+export default function About() {
+
+    return <div className={css.about}>
+            <h1>Sobre Starcards</h1>
+            <p>Starcards es un juego de cartas basado en el universo Starcraft. El reconocido juego de estrategia de ciencia ficción se recrea en un formato de juego de cartas en el que los participantes podrán comprar packs de cartas, armar sus mazos y competir con otros jugadores online eligiendo a sus personajes preferidos.</p>
+           <br/>
+           <p>Desarrollamos esta idea para el proyecto grupal final de la carrera de desarrollo full stack de Henry. Starcards surgió como idea de uno de los participantes y se fue potenciando con las experiencias y conocimientos de cada uno. El equipo lo conformamos Aldo Aliscioni, Antu D’Ippolito, Emerson Edward Villalta, Emiliano Aparicio, Fabián Santos, Lautaro Pérez y Manuel Losada.</p>
+           <br/>
+           <p>Para el desarrollo de Starcards utilizamos tecnologías aprendidas en Henry así como también nuevas herramientas en las que nos aventuramos para mejorar la experiencia de usuario. Sin embargo, Starcards no expresa solamente lo aprendido en términos de desarrollo web sino también la formación que tuvimos como fruto de un intenso trabajo colectivo durante algunas semanas. Este proceso, difícil de ver solamente navegando en nuestra página, fue fundamental para adquirir conocimientos de trabajo en equipo, planificación y flujo de trabajo.</p>
+           <br/>
+           <p>Esperamos que este producto les guste tanto como a nosotros. No duden en dejarnos su feedback para seguir mejorando.</p>
+           </div>
+}
---
+++ client/src/components/Rules/Rules.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import css from "./Rules.module.css";
+
+export default function Rules() {
+  return (
+    <div className={css.rules}>
+      <h1>Rules</h1>
+    </div>
+  );
+}
