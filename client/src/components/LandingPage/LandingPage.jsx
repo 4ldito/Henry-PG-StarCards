a7655b6a9e4dd1483bd292216ab7a6a1c1fcf156
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-09-01 23:32:50 -0300

    Arreglos en el responsive. LP

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import getAllCards from "../../redux/actions/cards/getAllCards";
 
 import { getUser } from "../../redux/actions/user";
 import logo from "../../img/logo-15.png";
@@ -10,24 +9,29 @@ import nave3 from "../../img/nave3.png";
 import prev from "../../img/prev.svg";
 import next from "../../img/next.svg";
 
+import Zeratul from "../../img/Cards landing/Zeratul.png";
+import Kerrigan from "../../img/Cards landing/Kerrigan.png";
+import Raynor from "../../img/Cards landing/Raynor.png";
+import Carrier from "../../img/Cards landing/Carrier.png";
+import Mutalisk from "../../img/Cards landing/Mutalisk.png";
+import Battlecruiser from "../../img/Cards landing/Battlecruiser.png";
+import Zealot from "../../img/Cards landing/Zealot.png";
+import Zergling from "../../img/Cards landing/Zergling.png";
+import Marine from "../../img/Cards landing/Marine.png";
+
 import css from "./landingPage.module.css";
-import Card from "../Card/Card";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function LandingPage() {
   const nave1ref = useRef(null);
   const [tamaño, setTamaño] = useState("50.5%");
-  const allCards = useSelector((state) => state.album.filteredCards);
+  const [limit, setLimit] = useState(1);
   const dispatch = useDispatch();
 
   const user = useSelector((state) => state.userReducer.user);
 
   useEffect(() => {
     if (user.id !== undefined) dispatch(getUser(user.id));
-  }, []);
-
-  useEffect(() => {
-    dispatch(getAllCards());
   }, []);
 
   useEffect(() => {
@@ -44,15 +48,27 @@ export default function LandingPage() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  function passImageNext() {
+    if (limit === 1) setLimit(2);
+    if (limit === 2) setLimit(3);
+    if (limit === 3) setLimit(1);
+  }
+
+  function passImageBack() {
+    if (limit === 1) setLimit(3);
+    if (limit === 2) setLimit(1);
+    if (limit === 3) setLimit(2);
+  }
   return (
     <>
       <div className={css.containerLandingPage}>
         <div className={css.LandingPage}>
           <img className={css.logo} src={logo} alt="Logo" />
           <div>
-            <h1>EL JUEGO DE CARTAS DEL UNIVERSO STARCRAFT</h1>
+            <h1>THE STARCRAFT UNIVERSE CARD GAME</h1>
           </div>
-          <button className={css.btn}>JUGAR</button>
+          <button className={css.btn}>PLAY</button>
         </div>
         <img
           ref={nave1ref}
@@ -67,42 +83,48 @@ export default function LandingPage() {
         <div className={css.containerTitle}>
           <img className={css.nave2} src={nave2} alt="" />
           <h1>
-            JUGA ONLINE CONTRA OTROS JUGADORES USANDO TUS PERSONAJES FAVORITOS
+            PLAY ONLINE AGAINST OTHER PLAYERS USING YOUR FAVORITE CHARACTERS
           </h1>
         </div>
-        <div className={css.containerCardsTo}>
-          <div className={css.containerCards}>
+        <div className={css.containerCards}>
+          <div className={css.cards}>
             <div className={css.arrow}>
-              <img src={prev} alt="" />
+              <img src={prev} alt="" onClick={passImageBack} />
             </div>
-            <div className={css.cards}>
-              {allCards.map((card, index) => {
-                return (
-                  <Card
-                    tamanho={1}
-                    key={card.id}
-                    id={card.id}
-                    name={card.name}
-                    image={card.image}
-                    cost={card.cost}
-                    Gdmg={card.Gdmg}
-                    Admg={card.Admg}
-                    life={card.life}
-                    ability={card.ability}
-                    abilities={card.abilities}
-                    race={card.race}
-                    movement={card.movement}
-                  />
-                );
-              })}
-            </div>
+            {limit === 1 && (
+              <>
+                <img className={css.card} src={Zeratul} alt="Zeratul" />
+                <img className={css.card} src={Kerrigan} alt="Kerrigan" />
+                <img className={css.card} src={Raynor} alt="Raynor" />
+              </>
+            )}
+
+            {limit === 2 && (
+              <>
+                <img className={css.card} src={Carrier} alt="Carrier" />
+                <img className={css.card} src={Mutalisk} alt="Mutalisk" />
+                <img
+                  className={css.card}
+                  src={Battlecruiser}
+                  alt="Battlecruiser"
+                />
+              </>
+            )}
+
+            {limit === 3 && (
+              <>
+                <img className={css.card} src={Zealot} alt="Zealot" />
+                <img className={css.card} src={Zergling} alt="Zergling" />
+                <img className={css.card} src={Marine} alt="Marine" />
+              </>
+            )}
             <div className={css.arrow}>
-              <img src={next} alt="" />
+              <img src={next} alt="" onClick={passImageNext} />
             </div>
           </div>
         </div>
         <div className={css.containerTitleDown}>
-          <h1>ELEGÍ PROTOSS, TERRAN O ZERG PARA ARMAR TU MAZO Y COMPETIR</h1>
+          <h1>CHOOSE PROTOSS, TERRAN OR ZERG TO BUILD YOUR DECK AND COMPETE</h1>
         </div>
         <div className={css.containerCards}>
           <img className={css.imageLanding} src={imageLanding} alt="" />
