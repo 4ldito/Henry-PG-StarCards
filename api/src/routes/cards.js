5008b6f85df0773dcb9bdcfad9a29d98f6d38560
Emerson Villalta <reaperfocs@gmail.com>
2022-09-06 12:43:10 -0500

    post cards

--- api/src/routes/cards.js
+++ api/src/routes/cards.js
@@ -34,4 +34,37 @@ cardsRoute.get("/:status", async (req, res, next) => {
   }
 });
 
+cardsRoute.post("/", async(req,res, next)=>{
+  const {name, Gdmg, Admg, life, ability, abilities, race, cost, movement, image} = req.body;
+  //name:name.charAt(0).toUpperCase()+name.slice(1);
+  try {
+    if (name) {
+      const isCard = await Card.findOne({where: {name}});
+      //console.log(isCard.name.toLowerCase());
+      if(isCard){
+        return res.status(400).send('ya existe')
+      }else{
+        const newCard = await Card.findOrCreate({
+          where:{
+            name,
+            Gdmg,
+            Admg,
+            life,
+            ability,
+            abilities,
+            race,
+            cost,
+            movement,
+            image
+          }
+        }) 
+        return res.status(201).send(newCard);
+      }
+    }
+  } catch (error) {
+    return next(error)
+  }
+})
+
+
 module.exports = cardsRoute;
