1ee539d3de35271965fe893cdea706d893561e5b
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-09-07 16:33:18 -0300

    now score saved

--- api/src/socketServer.js
+++ api/src/socketServer.js
@@ -202,7 +202,7 @@ io.on("connection", (socket) => {
 
       const newPlayer = await User.findOne({
         where: { id: playerId },
-        attributes: ["id", "username", "onGame", "defaultDeck"],
+        attributes: ["id", "username", "onGame", "defaultDeck", "score"],
       });
 
       if (!newPlayer.onGame) {
@@ -266,13 +266,25 @@ io.on("connection", (socket) => {
           gameExe(p1Deck, p2Deck).then(async (gameInfo) => {
             const game = await Game.create({ info: gameInfo });
 
-            await Promise.all([
-              Promise.all([p1.addGame(game), p2.addGame(game)]),
-              Promise.all([
-                p1.update({ onGame: false }),
-                p2.update({ onGame: false }),
-              ]),
-            ]);
+            try {
+              console.log("score p1", p1.score)
+              console.log("score p2", p2)
+              await Promise.all([
+                Promise.all([p1.addGame(game), p2.addGame(game)]),
+                Promise.all([
+                  p1.update({
+                    onGame: false,
+                    score: game.info.winner === p1.id ? p1.score + 1 : p1.score,
+                  }),
+                  p2.update({
+                    onGame: false,
+                    score: game.info.winner === p2.id ? p1.score + 1 : p2.score,
+                  }),
+                ]),
+              ]);
+            } catch (error) {
+              console.log(error)
+            }
 
             if (p1Socket) io.to(p1Socket).emit("gameResults", "Game finished");
             if (p2Socket) io.to(p2Socket).emit("gameResults", "Game finished");
