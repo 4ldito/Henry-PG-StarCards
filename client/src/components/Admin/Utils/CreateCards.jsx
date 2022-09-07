d90478c11eec743586eb7e436fc7424789022a86
Emerson Villalta <reaperfocs@gmail.com>
2022-09-07 15:32:08 -0500

    create cards v2

--- client/src/components/Admin/Utils/CreateCards.jsx
+++ client/src/components/Admin/Utils/CreateCards.jsx
@@ -5,9 +5,15 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import createCardsAdmin from './../../../redux/actions/cards/createCards'
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-
+import { useRef } from 'react';
+import style from './CreatePacks.module.css'
 
 function CreateCards() {
+    const name2 = useRef(null);
+    const price = useRef(null);
+  
+
+
     async function uploadFilePack(file, race, name) {
         const storageRef = ref(storage, `cards/${race}/${name}`); //nombre de ref para la subida
         await uploadBytes(storageRef, file); //subida del archivo
@@ -18,6 +24,7 @@ function CreateCards() {
     const dispatch = useDispatch();
 
     //hooks
+    const [errors, setErrors] = useState(null);
     //files
     const [file, setFile] = useState(null);
 
@@ -34,132 +41,149 @@ function CreateCards() {
         image: null
     })
 
-    const handleChange = (e)=>{
-        setInput({
+    const handleChange = (e) => {
+        let name = name2.current.value;
+        setErrors("");
+        if (name) {
+          if (/^[A-Z]+$/i.test(name)) {
+            setInput({
+              ...input,
+              [e.target.name]: e.target.value,
+            });
+          } else {
+            setErrors("solo letras");
+          }
+        } else {
+          setInput({
             ...input,
             [e.target.name]: e.target.value,
-        });
-    }
-
-    const handleSelectRace = (e)=>{
-        if(input.race.includes(e.target.value)){
-            alert('Otra raza');
-        }else{
-            if(input.race.includes(e.target.value)){
-                alert('Otra raza literal')
-            }else{
-                setInput({
-                    ...input,
-                    race: [...input.race, e.target.value],
-                });
-            }
+          });
         }
-    }
+    };
+
+    const handleSelectRace = (e) => {
+        if (input.race.includes(e.target.value)) {
+          alert("Otra raza");
+        } else {
+          setInput({
+            ...input,
+            race: e.target.value,
+          });
+        }
+    };
+    
 
     const handleSubmitPack = async (e) => {
         e.preventDefault();
+    
         try {
-        const result = await uploadFilePack(file, input.name);//obteninedo la url con el nombre
-            //console.log(result);
-        input.image = result//obteniendo en el input.image el url
-        /*const valorespack = input
-        valorespack.percent = valorespack.percent.map((perce)=>perce/100) //división a 100
-
-        for (let i = 0; i < valorespack.percent.length; i++) {//agregado el porcentaje
-          valorespack.cards[i].push(valorespack.percent[i])
-        }
-        valorespack.percent=null//anulando percent
-        */
-        console.log(valorespack);
-        dispatch(createPackCardsAdmin(valorespack)) //enviando los valores reales
+          if (!errors) {
+            const result = await uploadFilePack(file, input.race,input.name); //obteninedo la url con el nombre
+            input.image = result; //obteniendo en el input.image el url
+            const valorespack = input;
+            // valorespack.percent = valorespack.percent.map((perce) => perce / 100); //división a 100
+    
+            // for (let i = 0; i < valorespack.percent.length; i++) {
+            //   //agregado el porcentaje
+            //   valorespack.cards[i].push(valorespack.percent[i]);
+            // }
+            // valorespack.percent = null; //anulando percent
+            console.log(valorespack);
+            dispatch(createPackCardsAdmin(valorespack));
+            setInput({
+              name: "",
+              price: "",
+              race: "",
+              cards: [],
+              // percent: [],
+              //stock: "",
+              image: null,
+              //amount: "",
+            });
+          } else {
+            alert("Name incorrect");
+          }
         } catch (error) {
-            alert('intentelo otra vez');
+          alert("Retry");
         }
+      };
+
+    
+    const handleDelete = (e) => {
+        setInput({
+            ...input,
+            cards: input.cards.filter((card) => card !== e),
+        });
     };
 
     return(
         <>
-      <h1>New Pack</h1>
+      <div className={style.container}>
+        <h1>New Pack</h1>
         <form onSubmit={handleSubmitPack}>
-        <input
-          type="text"
-          name="name"
-          value={input.name}
-          onChange={(e) => handleChange(e)}
-          placeholder="Name card"
-        />
-        <input
-          type="number"
-          name="price"
-          min="1"
-          max="3000"
-          value={input.price}
-          onChange={(e) => handleChange(e)}
-          placeholder="Precio"
-        />
-        <input
-          type="text"
-          name="amount"
-          value={input.amount}
-          onChange={(e) => handleChange(e)}
-          placeholder="Amount"
-        />
-        <input
-          type="number"
-          name="stock"
-          value={input.stock}
-          onChange={(e) => handleChange(e)}
-          placeholder="stock"
-        />
-        <div>
-          <select 
-            onChange={(e)=>handleSelectRace(e)}
-          >
-            <option hidden={true} key="raza">Raza:</option>
-            <option value="Zerg" key="Zerg">Zerg</option>
-            <option value="Terran" key="Terran">Terran</option>
-            <option value="Protoss" key="Protoss">Protoss</option>
-          </select>
-        </div>
-        
-        <input
-          type="file"
-          name=""
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <button>new Pack</button>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => handleChange(e)}
+            placeholder="Name pack"
+            required
+            ref={name2}
+            min="5"
+          />
+          {errors && <p>{errors}</p>}
+          <input
+            type="number"
+            name="price"
+            min="1"
+            max="3000"
+            onChange={(e) => handleChange(e)}
+            placeholder="Precio"
+            required
+            ref={price}
+          />
+          <input
+            type="number"
+            name="amount"
+            onChange={(e) => handleChange(e)}
+            placeholder="Amount"
+            required
+            //ref={amount}
+            min="1"
+          />
+          <input
+            type="number"
+            name="stock"
+            onChange={(e) => handleChange(e)}
+            placeholder="stock"
+            required
+            //ref={stock}
+            min="1"
+          />
+          <div>
+            <label>Race: </label>
+            <select onChange={(e) => handleSelectRace(e)} required>
+              <option hidden={true} key="raza"></option>
+              <option value="Zerg" key="Zerg">
+                Zerg
+              </option>
+              <option value="Terran" key="Terran">
+                Terran
+              </option>
+              <option value="Protoss" key="Protoss">
+                Protoss
+              </option>
+            </select>
+          </div>
 
+          <input
+            type="file"
+            name=""
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <button>Create</button>
 
-        <div>
-        {input.cards?.map((e)=>{
-          return(
-            <div key={e}>
-              <p>{e}</p>
-              <select onChange={(e)=>handleSelectPercent(e)}>
-                    <option hidden={true}>Porcentaje</option>
-                    <option value="1">1%</option>
-                    <option value="2">2%</option>
-                    <option value="3">3%</option>
-                    <option value="5">5%</option>
-                    <option value="7">7%</option>
-                    <option value="10">10%</option>
-                    <option value="15">15%</option>
-                    <option value="20">20%</option>        
-              </select>
-              <button 
-                onClick={()=>{
-                  handleDelete(e) 
-                }}
-              >
-                x
-              </button>
-            </div>
-          )
-        })}
+        </form>
       </div>
-      </form>
-
-
     </>
     )
 }
