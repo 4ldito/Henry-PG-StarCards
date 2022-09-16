0a500e042a303efdb08bbef43061f965aeb9d81a
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-09-16 16:26:45 -0300

    Arreglos en el About. LP

--- client/src/components/About/team.js
+++ client/src/components/About/team.js
@@ -1,17 +1,13 @@
 export const team = [
   {
     name: "Aldo Aliscioni",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQGrPAgH_gNjkg/profile-displayphoto-shrink_800_800/0/1657661990019?e=1668038400&v=beta&t=mk87MXPtRy_41xZEeOQcXbH7sp200q46WHhAEZdKavg",
+      "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/perfil?alt=media&token=eba4d67c-2e13-4311-badc-919bb1d60c85",
     linkedin: "https://www.linkedin.com/in/aldo-aliscioni/",
     github: "https://github.com/4ldito",
   },
   {
     name: "Antu D’Ippolito",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
       "https://media-exp1.licdn.com/dms/image/C4D03AQE-XITGuCxpsA/profile-displayphoto-shrink_800_800/0/1657677264546?e=1668038400&v=beta&t=fA3PvW12CXzlydfkanj3YcTK15MUBEMzfRH2qzivTKY",
     linkedin: "https://www.linkedin.com/in/antu-dippolito-a5892823b/",
@@ -19,8 +15,6 @@ export const team = [
   },
   {
     name: "Emerson Edward Villalta",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
       "https://media-exp1.licdn.com/dms/image/C4D03AQEA1sxUoNMoxg/profile-displayphoto-shrink_200_200/0/1630822394567?e=1668038400&v=beta&t=EC5LpBNR1a9SwTBEt3CM-eLwFI8U1IQgDh91dT47-FI",
     linkedin:
@@ -29,35 +23,27 @@ export const team = [
   },
   {
     name: "Emiliano Aparicio",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/packs%2FBunker%20Terran?alt=media&token=7741703c-7122-4d4f-9911-cfb84c6244d3",
+      "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/perfil?alt=media&token=f28e62c9-152c-45ba-906e-b06dbf9e570d",
     linkedin: "https://www.linkedin.com/in/emiliano-aparicio-8b9757236/",
     github: "https://github.com/NoahReaver",
   },
   {
     name: "Fabián Santos",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQHUmPcXtQkHkA/profile-displayphoto-shrink_200_200/0/1656376942352?e=1668038400&v=beta&t=dsnihevzSuqmpcziXWb0akCf_Eb1PEJUYgrze-h16Vk",
+      "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/perfil?alt=media&token=befaf69f-67d9-4bd8-9348-a71b327b9788",
     linkedin: "https://www.linkedin.com/in/santosfabian/",
     github: "https://github.com/fabian179rc",
   },
   {
     name: "Lautaro Perez",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEKKtC-u5y9lw/profile-displayphoto-shrink_200_200/0/1662230329267?e=1668038400&v=beta&t=yeprTthllwT-s7RUC-RpN-PDnwzQSHBepGJqKgiwwpg",
+      "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/perfil?alt=media&token=b791ac2d-d962-49c7-bd88-e4b95e94de38",
     linkedin: "https://www.linkedin.com/in/lautaroperez135/",
     github: "https://github.com/lauty-shock",
   },
   {
     name: "Manuel Losada",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
       "https://media-exp1.licdn.com/dms/image/C4E03AQGn1ihS2OBcHw/profile-displayphoto-shrink_200_200/0/1544454843743?e=1668038400&v=beta&t=Vsk8mjn3WebWEPfNa7BN16wmkhyBdw22qXd58du35xI",
     linkedin: "https://www.linkedin.com/in/ManuelLosada/",
