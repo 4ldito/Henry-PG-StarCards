80c4deed5d87791a40cd875a9faf5896c4065b04
Fabian179rc <fabian179rc@gmail.com>
2022-12-20 10:38:26 -0300

    actualizacion img

--- client/src/components/About/team.js
+++ client/src/components/About/team.js
@@ -1,36 +1,28 @@
 export const team = [
   {
     name: "Aldo Aliscioni",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQGrPAgH_gNjkg/profile-displayphoto-shrink_800_800/0/1657661990019?e=1668038400&v=beta&t=mk87MXPtRy_41xZEeOQcXbH7sp200q46WHhAEZdKavg",
+      "https://media.licdn.com/dms/image/D4D35AQGTHTsxrtItmA/profile-framedphoto-shrink_800_800/0/1657662416826?e=1672149600&v=beta&t=hA5wJrW0rstmgGpl5SJtUJ5oh4Akk9KFY_2UO0rgGF8",
     linkedin: "https://www.linkedin.com/in/aldo-aliscioni/",
     github: "https://github.com/4ldito",
   },
   {
     name: "Antu D’Ippolito",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQE-XITGuCxpsA/profile-displayphoto-shrink_800_800/0/1657677264546?e=1668038400&v=beta&t=fA3PvW12CXzlydfkanj3YcTK15MUBEMzfRH2qzivTKY",
+      "https://media.licdn.com/dms/image/D4D35AQECfWbnBJdlgw/profile-framedphoto-shrink_800_800/0/1663116911521?e=1672149600&v=beta&t=Z7z59oWfY3ClakhcnYHF3hyuH44vxu-Uhnn9n-67x58",
     linkedin: "https://www.linkedin.com/in/antu-dippolito-a5892823b/",
     github: "https://github.com/Antudip",
   },
   {
     name: "Emerson Edward Villalta",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEA1sxUoNMoxg/profile-displayphoto-shrink_200_200/0/1630822394567?e=1668038400&v=beta&t=EC5LpBNR1a9SwTBEt3CM-eLwFI8U1IQgDh91dT47-FI",
+      "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/packs%2FBunker%20Terran?alt=media&token=7741703c-7122-4d4f-9911-cfb84c6244d3",
     linkedin:
       "https://www.linkedin.com/in/emerson-edward-villalta-quispe-1b09a01aa/",
     github: "https://github.com/Blacksavior24",
   },
   {
     name: "Emiliano Aparicio",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
       "https://firebasestorage.googleapis.com/v0/b/starcardsv2.appspot.com/o/packs%2FBunker%20Terran?alt=media&token=7741703c-7122-4d4f-9911-cfb84c6244d3",
     linkedin: "https://www.linkedin.com/in/emiliano-aparicio-8b9757236/",
@@ -38,28 +30,22 @@ export const team = [
   },
   {
     name: "Fabián Santos",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQHUmPcXtQkHkA/profile-displayphoto-shrink_200_200/0/1656376942352?e=1668038400&v=beta&t=dsnihevzSuqmpcziXWb0akCf_Eb1PEJUYgrze-h16Vk",
+      "https://media-exp1.licdn.com/dms/image/D5603AQG9t4gjnNlj_A/profile-displayphoto-shrink_400_400/0/1667835217955?e=1675296000&v=beta&t=6e9Nf2gm6FZqmJlw0sPu9XJG7f57Cp-OWXBOG2-juqE",
     linkedin: "https://www.linkedin.com/in/santosfabian/",
     github: "https://github.com/fabian179rc",
   },
   {
     name: "Lautaro Perez",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEKKtC-u5y9lw/profile-displayphoto-shrink_200_200/0/1662230329267?e=1668038400&v=beta&t=yeprTthllwT-s7RUC-RpN-PDnwzQSHBepGJqKgiwwpg",
+      "https://media.licdn.com/dms/image/D4D35AQFNOj3atMNL-A/profile-framedphoto-shrink_800_800/0/1668028809304?e=1672149600&v=beta&t=9YcGzHe-sBLnBQheEAR9NMs7oIWKG3t9lfV0UT5yUBc",
     linkedin: "https://www.linkedin.com/in/lautaroperez135/",
     github: "https://github.com/lauty-shock",
   },
   {
     name: "Manuel Losada",
-    summary:
-      "De Córdoba, Argentina, Blalasddbasdpas alsknda la   ahon aolh lal alnlbua nlbk ahpb aoibn aoub oasdnjhaslkdna olihndasoidhnaisdjnhaolsihdnpañ anbdoasilkdn",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQGn1ihS2OBcHw/profile-displayphoto-shrink_200_200/0/1544454843743?e=1668038400&v=beta&t=Vsk8mjn3WebWEPfNa7BN16wmkhyBdw22qXd58du35xI",
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGn1ihS2OBcHw/profile-displayphoto-shrink_400_400/0/1544454843743?e=1675296000&v=beta&t=56qD-HxphIXIS9pmOGVWMlbbj0fKDMYmeBde49GBhJs",
     linkedin: "https://www.linkedin.com/in/ManuelLosada/",
     github: "https://github.com/manuellosada90",
   },
